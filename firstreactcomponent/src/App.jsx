a1df35faaf117aeda9a5a1fc6762795e1bb479fb
suchitrasahu996-creator <suchitrasahu996@gmail.com>
2025-12-15 05:32:57 +0530

    added the codes

---
+++ firstreactcomponent/src/App.jsx
@@ -0,0 +1,10 @@
+import FirstComponent from "./FirstComponent";
+
+function App(){
+  return(
+    <div>
+      <FirstComponent/>
+    </div>
+  );
+}
+export default App;
---
+++ firstreactcomponent/src/FirstComponent.jsx
@@ -0,0 +1,6 @@
+function FirstComponent(){
+    return (
+        <h2> This is my First React Component</h2>
+    );
+}
+export default FirstComponent;
